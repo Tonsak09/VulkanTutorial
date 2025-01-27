26f4c4cad75c0ae1457392b0e97ac37af042c3fd
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2025-01-27 18:19:43 -0500

    Image views

--- Vulkan_Tutorial/Main.cpp
+++ Vulkan_Tutorial/Main.cpp
@@ -85,6 +85,8 @@ private:
     
 
     VkSwapchainKHR swapChain;
+    // Views let us access the images 
+    std::vector<VkImageView> swapChainImageViews;
     std::vector<VkImage> swapChainImages; 
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
@@ -843,6 +845,47 @@ private: // Vukan helpers
 
     #pragma endregion
 
+    #pragma region Image Views 
+
+    void CreateImageViews()
+    {
+        swapChainImageViews.resize(swapChainImages.size());
+
+        for (size_t i = 0; i < swapChainImages.size(); i++)
+        {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+
+            // How should the data be interpreted? 
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            // Allow for color swizzling of each component 
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            // Image purpose and how it should be accessed 
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("Failed to create image views!"); 
+            }
+
+            // View is ready for texture use but not quite ready
+            // to be used a render target yet! 
+        }
+    }
+
+    #pragma endregion
+
 private: // Main functions 
     void InitWindow()
     {
@@ -879,6 +922,13 @@ private: // Main functions
 
     void Cleanup() 
     {
+        // Since we created the image views we need to manually 
+        // destroy all of them 
+        for (auto imageView : swapChainImageViews)
+        {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
 
